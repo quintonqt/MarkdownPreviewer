9961286bce7999bb7504893da9915a9a41aa6b84
Quinton Tran <quintonqt@gmail.com>
2020-07-20 14:17:09 -0400

    Added feature where the previewer shows the text changes made in the editor

--- src/App.js
+++ src/App.js
@@ -5,24 +5,34 @@ import './App.css';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			input: 'Bob',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
 	}
 
+	handleChange(event) {
+		this.setState({
+			input: event.target.value,
+		});
+	}
 	render() {
 		return (
 			<div>
-				<Editor />
-				<Previewer />
+				<Editor onChange={this.handleChange} />
+				<Previewer text={this.state.input} />
 			</div>
 		);
 	}
 }
 
-const Editor = () => {
-	return <textarea id="editor"></textarea>;
+const Editor = (props) => {
+	return <textarea id="editor" onChange={props.onChange} />;
 };
 
-const Previewer = () => {
-	return <div id="preview"></div>;
+const Previewer = (props) => {
+	return <div id="preview">{props.text}</div>;
 };
 
 export default App;
