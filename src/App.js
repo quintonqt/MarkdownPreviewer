a67d080bb918f6435b62e8c884d1c17d56a5f41c
Quinton Tran <quintonqt@gmail.com>
2020-07-20 16:41:34 -0400

    Completed previewer skeleton and added default markdown to render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			input: '',
+			input: defaultText,
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -20,7 +20,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Editor onChange={this.handleChange} />
+				<Editor value={this.state.input} onChange={this.handleChange} />
 				<Previewer text={marked(this.state.input)} />
 			</div>
 		);
@@ -28,7 +28,11 @@ class App extends React.Component {
 }
 
 const Editor = (props) => {
-	return <textarea id="editor" onChange={props.onChange} />;
+	return (
+		<textarea id="editor" onChange={props.onChange}>
+			{props.value}
+		</textarea>
+	);
 };
 
 const Previewer = (props) => {
@@ -38,3 +42,29 @@ const Previewer = (props) => {
 };
 
 export default App;
+
+const defaultText = `# This Markdown Previewer was made with React
+
+## This is a sub-header
+
+Check out my website [here](https://quintonqt.github.io)
+
+\`Here is some pseudocode inline\`
+
+Here is a block of code:
+\`\`\`
+	<div>
+		<Editor value={this.state.input} onChange={this.handleChange} />
+		<Previewer text={marked(this.state.input)} />
+	</div>
+\`\`\`
+
+* Nani?
+
+> This is a block quote. Just add the greater than symbol in front 
+of whatever you want to write!
+
+![React Logo](./logo192.png)
+
+**BE BOLD**
+`;
